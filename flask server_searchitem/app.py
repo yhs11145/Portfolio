8c6c1d0f009b5a9e600af4f66e7dbd93c15c5bdc
yhs11145 <yhs11145@naver.com>
2021-05-22 14:50:00 +0900

    flask_templates 기본구성 작성

--- flask server_searchitem/app.py
+++ flask server_searchitem/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Flask'
+    return render_template('admin.html')
     
 @app.route('/info')
 def info():
