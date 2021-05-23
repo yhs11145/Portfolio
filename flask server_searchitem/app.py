385d47142d54a31d6037f64406c1561d42e8a128
yhs11145 <yhs11145@naver.com>
2021-05-23 16:09:17 +0900

    네이버 쇼핑 검색 크롤링 완료

--- flask server_searchitem/app.py
+++ flask server_searchitem/app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from flask.wrappers import Response
+import requests
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -10,6 +13,17 @@ def index():
 @app.route('/info')
 def info():
     return 'Info'
+
+@app.route('/search')
+def search():
+    search="마스크"
+    values={
+          'query': search
+          }
+    response=requests.get('https://shopping.naver.com/search/all?',params=values)##네이버
+    print(response.url)
+    soup=BeautifulSoup(response.text,"html.parser")
+    return soup.text
 
 
 if __name__=="__main__":
